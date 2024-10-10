a017ca86f31137df73926181ce53cc1d19400878
techiemokhele <neomokhele23@gmail.com>
2024-10-10 20:29:07 +0200

    Initial project setup, folder & structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        gray: {
+          1: "#B4CAD5",
+          2: "#B4CAD580",
+          3: "#262A2D",
+        },
+        gold: {
+          1: "#F4BF31"
+        },
+        iconColor: {
+          1: "#C6CACD"
+        },
+        gradient: {
+          1: "#FFFFFF66",
+          2: "#EEEDED33"
+        }
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
